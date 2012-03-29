9f67e9811beaf53a60c0e10184609bccabd852d1
Nick Grossman <nick@wrkng.net>
2012-03-28 23:47:52 -0400

    add bootstrap; make customizations for netizen

---
+++ wordpress-bootstrap-v2.1/footer.php
@@ -0,0 +1,82 @@
+			<footer role="contentinfo">
+			
+				<div id="inner-footer" class="clearfix">
+		          <hr />
+		          <div id="widget-footer" class="clearfix row-fluid">
+		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
+		            <?php endif; ?>
+		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
+		            <?php endif; ?>
+		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
+		            <?php endif; ?>
+		          </div>
+					
+					<nav class="clearfix">
+						<?php bones_footer_links(); // Adjust using Menus in Wordpress Admin ?>
+					</nav>
+					
+				<!-- Begin MailChimp Signup Form -->
+<form class="form-inline validate" action="http://usv.us4.list-manage.com/subscribe/post?u=8421859f947e29f79daa069a1&amp;id=982a0efde2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
+  <label>Stay in touch: &nbsp;&nbsp;</label>
+  <input type="text" class="input-large" placeholder="Email" id="mce-EMAIL" name="EMAIL">
+  <button type="submit" name="subscribe" class="btn" id="mc-embedded-subscribe">Go</button>
+</form>
+
+			
+					<!--<p class="attribution">&copy; <?php bloginfo('name'); ?></p>-->
+				
+				</div> <!-- end #inner-footer -->
+				
+			</footer> <!-- end footer -->
+		
+		</div> <!-- end #container -->
+		
+		<!-- scripts are now optimized via Modernizr.load -->	
+		<script src="<?php echo get_template_directory_uri(); ?>/library/js/scripts.js"></script>
+		
+		<!--[if lt IE 7 ]>
+  			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+  			<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+		<![endif]-->
+		
+		    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+		<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
+		<!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>-->
+    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.7.1.min.js"></script>
+
+		<script>window.jQuery || document.write(unescape('%3Cscript src="<?php echo get_template_directory_uri(); ?>/library/js/libs/jquery-1.7.1.min.js"%3E%3C/script%3E'))</script>
+		
+		<script src="<?php echo get_template_directory_uri(); ?>/library/js/modernizr.full.min.js"></script>
+		
+		<!-- media-queries.js (fallback) -->
+		<!--[if lt IE 9]>
+			<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>			
+		<![endif]-->
+
+		<!-- html5.js -->
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->    
+    
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap-transition.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap-alert.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap-modal.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap-dropdown.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap-scrollspy.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap-tab.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap-tooltip.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap-popover.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap-button.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap-collapse.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap-carousel.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap-typeahead.js"></script>
+    <!--<script src="<?php echo get_template_directory_uri(); ?>/js/less-1.2.1.min.js"></script>-->
+		
+		<?php wp_footer(); // js scripts are inserted using this function ?>
+
+	</body>
+
+</html>
---
+++ wordpress-bootstrap-v2.1/header.php
@@ -0,0 +1,92 @@
+<!doctype html>  
+
+<!--[if IEMobile 7 ]> <html <?php language_attributes(); ?>class="no-js iem7"> <![endif]-->
+<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+	
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		
+		<title><?php bloginfo('title'); ?></title>
+				
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+		
+		<!-- icons & favicons -->
+		<!-- For iPhone 4 -->
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/h/apple-touch-icon.png">
+		<!-- For iPad 1-->
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/m/apple-touch-icon.png">
+		<!-- For iPhone 3G, iPod Touch and Android -->
+		<link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/l/apple-touch-icon-precomposed.png">
+		<!-- For Nokia -->
+		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/l/apple-touch-icon.png">
+		<!-- For everything else -->
+		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+
+		
+  		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+		
+		<!-- wordpress head functions -->
+		<?php wp_head(); ?>
+		<!-- end of wordpress head -->
+		
+		<!--<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css">-->
+		<link href="<?php echo get_template_directory_uri(); ?>/less/bootstrap.css" rel="stylesheet">
+		<link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap-responsive.min.css" rel="stylesheet/less">
+
+		
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+		
+		<?php 
+
+			// check wp user level
+			get_currentuserinfo(); 
+			// store to use later
+			global $user_level; 
+		
+		?>
+				
+	</head>
+	
+	<body <?php body_class(); ?>>
+				
+		<header role="banner">
+		
+			<div id="inner-header" class="clearfix">
+				
+				<div class="navbar navbar-fixed-top">
+					<div class="navbar-inner">
+						<div class="container nav-container">
+							<nav role="navigation">
+								<!--<a class="brand" id="logo" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>-->
+								
+								<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+							        <span class="icon-bar"></span>
+							        <span class="icon-bar"></span>
+							        <span class="icon-bar"></span>
+								</a>
+								
+								<div class="nav-collapse">
+									<?php bones_main_nav(); // Adjust using Menus in Wordpress Admin ?>
+								</div>
+								
+							</nav>
+							
+							<?php if(of_get_option('search_bar', '1')) {?>
+							<form class="navbar-search pull-right" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+								<input name="s" id="s" type="text" class="search-query" placeholder="<?php _e('Search','bonestheme'); ?>">
+							</form>
+							<?php } ?>
+							
+						</div>
+					</div>
+				</div>
+			
+			</div> <!-- end #inner-header -->
+		
+		</header> <!-- end header -->
+		
+		<div class="container">
