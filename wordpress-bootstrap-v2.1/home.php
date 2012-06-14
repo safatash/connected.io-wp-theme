82282cf09eb8503374349a375578303997779a3e
Nick Grossman <nick@wrkng.net>
2012-06-14 11:23:33 -0400

    making the home page more nicer

--- wordpress-bootstrap-v2.1/home.php
+++ wordpress-bootstrap-v2.1/home.php
@@ -25,46 +25,66 @@
 				</ul>-->
 				
 				
-				<div class="well" id="carousel-container">
-				<div id="myCarousel" class="carousel slide">
-          <!-- Carousel items -->
-          <div class="carousel-inner">
-          
-          <?php
-        		$query_args = array( 'suppress_filters' => false, 'post_type' => 'quote', 'showposts' => '1000' );
-        		$slides = get_posts( $query_args );
-        		if ( ! empty( $slides ) ) {
-      			$counter = 0;
-      			foreach( $slides as $post ) { setup_postdata( $post ); $counter++;
-      		?>	
-          
-            <div class="item<?php if ($counter == 1) echo ' active'; ?>">
-              <blockquote>"<?php the_content_rss(); ?>"</blockquote>
-              <cite>&mdash; <?php the_excerpt_rss(); ?></cite>
-            </div>
-            
-            <?php } } ?>
-            
-          </div>
-          <!-- Carousel nav -->
-          <!--
-          <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-          <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
-        -->
-        </div>
-    </div>
-    
     <div class="row">
 	    <div class="span6">
-		    We believe in the net
+		    <?php
+		    $tmp_post = $post;
+		    $query_args = array( 'suppress_filters' => false, 'post_type' => 'page', 'name' => 'intro' );
+		    $pages = get_posts( $query_args );
+		    if ( ! empty( $pages ) ) :
+		    	setup_postdata( $pages[0] );
+		    ?>
+		    
+		    <h2 id="intro">We believe in the Net.</h2>
+		    <?php the_content(); ?>
+		
+			<?php 
+			endif; 
+			$post = $tmp_post;
+			?>	
+		    		
+		   
 		</div><!--/.span-->
 		<div class="span6">
-	    	Projects
+			
+	    	<iframe src="http://app.sliderocket.com:80/app/fullplayer.aspx?id=57a7d7bf-8efc-4401-bab3-2a86fdecabe6" width="458" height="370" scrolling=no frameBorder="0" style="border:1px solid #ddd;"></iframe>
 	    </div><!--/.span-->
 	    
 	</div><!--/.row -->
         
-		
+	<hr />
+	
+				<div class="well" id="carousel-container">
+				<div id="myCarousel" class="carousel slide">
+		  <!-- Carousel items -->
+		  <div class="carousel-inner">
+
+		  <?php
+				$query_args = array( 'suppress_filters' => false, 'post_type' => 'quote', 'showposts' => '1000' );
+				$slides = get_posts( $query_args );
+				if ( ! empty( $slides ) ) {
+	  			$counter = 0;
+	  			foreach( $slides as $post ) { setup_postdata( $post ); $counter++;
+	  		?>	
+
+			<div class="item<?php if ($counter == 1) echo ' active'; ?>">
+			  <blockquote>"<?php the_content_rss(); ?>"</blockquote>
+			  <cite>&mdash; <?php the_excerpt_rss(); ?></cite>
+			</div>
+
+			<?php } } ?>
+
+		  </div>
+		  <!-- Carousel nav -->
+		  <!--
+		  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+		  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+		-->
+		</div>
+	</div>
+
+	<hr />
+	
 	<div class="row">
 
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
@@ -75,7 +95,7 @@
 						
 							<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'wpbs-featured' ); ?></a>
 							
-							<div class="page-header"><h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1></div>
+							<div class="page-header"><h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3></div>
 							
 							<p class="meta"><time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_date(); ?></time> <?php _e("by", "bonestheme"); ?> <?php the_author_posts_link(); ?> </p>
 						
