33a739356fb09d402e7d5301c168746214a74704
Nick Grossman <nick@wrkng.net>
2012-06-16 08:41:29 -0400

    hide post footer

--- wordpress-bootstrap-v2.1/home.php
+++ wordpress-bootstrap-v2.1/home.php
@@ -114,12 +114,14 @@
 							<?php the_excerpt( __("Read more &raquo;","bonestheme") ); ?>
 						</section> <!-- end article section -->
 						
+						<?php /*
 						<footer>
 			
 							<p class="tags"><?php the_tags('<span class="tags-title">' . __("Tags","bonestheme") . ':</span> ', ' ', ''); ?></p>
 							
 						</footer> <!-- end article footer -->
-					
+						*/ ?>
+						
 					</article> <!-- end article -->
 					
 					<?php comments_template(); ?>
